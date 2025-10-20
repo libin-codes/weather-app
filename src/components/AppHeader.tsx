bb2a02791f9986d452305e1a805dab518adb6288
Libin Lalu <libinlalu000@gmail.com>
2025-10-20 11:11:52 +0530

    icons are now imported from lucide react

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,16 +1,15 @@
-import settingsIcon from "../assets/images/icon-units.svg"
-import dropDownIcon from "../assets/images/icon-dropdown.svg"
 import AppLogo from "../assets/images/logo.svg"
-
+import {Settings as SettingsIcon} from "lucide-react"
+import { ChevronDown as DropDownIcon } from 'lucide-react';
 
 export default function AppHeader() {
   return (
     <div className="flex justify-between">
         <img src={AppLogo} alt="" className="w-40"/>
-        <button className="flex gap-2 py-2 px-4 items-center bg-[#262540] rounded-md cursor-pointer">
-            <img src={settingsIcon} alt="" className="w-4 h-4"/>
-            <p className="text-white">Units</p>
-            <img src={dropDownIcon} alt="" className="w-3.5 h-3.5"/>
+        <button className="flex gap-2 py-2 px-4 items-center bg-[#262540] rounded-md cursor-pointer text-white">
+            <SettingsIcon/>
+            <p>Units</p>
+            <DropDownIcon/>
         </button>
     </div>
   )
